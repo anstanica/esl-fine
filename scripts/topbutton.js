d523efde733913023fb706597f67b3f6b6da248f
anstanica <anstanica@outlook.com>
2023-01-26 21:43:36 +0800

    more updates

--- scripts/topbutton.js
+++ scripts/topbutton.js
@@ -1,18 +1,19 @@
-
-
 // This is the code for the top button
 // When the user scrolls down 20px from the top of the document, show the button jquery
-var btn = $('.myTopBtn');
+$(function () {
+  // var btn = $('.myTopBtn');
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > 100) {
+      $(".myTopBtn").show();
+    } else {
+      $(".myTopBtn").hide();
+    }
+  });
 
-$(window).on("scroll",function() {
-  if ($(window).scrollTop() > 300) {
-    btn.show();
-  } else {
-    btn.hide();
-  }
+  $(".myTopBtn").on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "300");
+  });
 });
 
-btn.on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
-});
+// When the user scrolls down 20px from the top of the document, show the button
